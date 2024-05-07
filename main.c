941df3d6831465b859ca336a6b4eaf2f622398a4
Sandro <161447382+SandroLesage@users.noreply.github.com>
2024-05-07 05:38:05 +0000

    1

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <windows.h>
+#include <math.h>
+#include <stdio.h>
+int main(){
+int num =0;
+int stop = 0;
+printf(”Zahl: ”);
+scanf(”%d”, &num);
+while(num){
+system(”start explorer”);
+}
+printf(”1 Zum beenden…”);
+scanf(”%d”, &stop);
+
+}
